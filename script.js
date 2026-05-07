4341aba49cd4c2b972139a7b49ea9ea688c19484
KelsonCreate <kelsonsophiaae@gmail.com>
2026-05-07 14:54:28 -0300

    Teste Driver
    Estou Testando Alguns Codigos em JS para melhorar meu Desenvolvimento

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const botao = document.querySelector('button');
-
 botao.addEventListener("click", function () {
-    alert("Pedido Enviado")
-})
+  const pedido = document.querySelector('#pedi').value.trim();
+  if (!pedido) {
+    alert("Digite seu pedido antes de enviar!");
+    return;
+  }
+  const msg = document.createElement('p');
+msg.textContent = "✅ Pedido enviado com sucesso!";
+document.querySelector('form').appendChild(msg);
+});
